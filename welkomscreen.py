43ffc221e68749460ecb08e38941d363e3e6b043
ib-2030 <55064204+ib-2030@users.noreply.github.com>
2019-10-30 12:19:32 +0100

    Add files via upload

--- welkomscreen.py
+++ welkomscreen.py
@@ -3,14 +3,23 @@ from PIL import ImageTk, Image
 
 def toonBezoekersFrame():
     FirstPage.pack_forget()
-    bezoekersFrame.pack
+    bezoekersFrame.pack()
     inloggennaam_entry.grid(row=1, column=1, pady=5, padx=10)
     email_entry.grid(row=2, column=1, pady=5, padx=10)
     inloggennaam_label.grid(row=1, column=0, pady=5, padx=10)
     email_label.grid(row=2, column=0, pady=5, padx=10)
+    terug_button1.grid(row=3, column=2)
     root.geometry("300x100")
 
-    
+def terugBezoekersNaarFirstpage():
+    bezoekersFrame.pack_forget()
+    root.geometry("700x250")
+    welkom_label.pack(side=TOP, pady=10, padx=10)
+    bezoekers_button.pack(side=LEFT, pady=10, padx=10)
+    ghouder_button.pack(side=RIGHT, pady=10, padx=10)
+    FirstPage.pack()
+
+
 
 
     
@@ -30,13 +39,13 @@ label = Label(master=FirstPage, image = photo)
 label.image = photo
 label.pack(side=TOP, padx=10)
 
-
 bezoekersFrame = Frame(master=root)
 bezoekersFrame.pack(fill="both", expand=True)
 inloggennaam_entry = Entry(master=bezoekersFrame)
 email_entry = Entry(master=bezoekersFrame)
 inloggennaam_label = Label(master=bezoekersFrame, text="Naam")
 email_label = Label(master=bezoekersFrame, text="Email")
+terug_button1 = Button(master=bezoekersFrame, text="Terug", command=terugBezoekersNaarFirstpage)
 
 
 
@@ -47,10 +56,6 @@ bezoekers_button.pack(side=LEFT, pady=10, padx=10)
 ghouder_button = Button(master=FirstPage, text="Galleriehouder")
 ghouder_button.pack(side=RIGHT, pady=10, padx=10)
 FirstPage.pack()
-
-
-
-
 
 
 
