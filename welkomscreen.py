5712e04918dbf7a77027d5deb915b3bfc81984bd
ib-2030 <55064204+ib-2030@users.noreply.github.com>
2019-10-30 13:56:27 +0100

    bezoeker input is registered in a dictionary

--- welkomscreen.py
+++ welkomscreen.py
@@ -1,5 +1,6 @@
 from tkinter import *
-from PIL import ImageTk, Image 
+from PIL import ImageTk, Image
+from tkinter.messagebox import showinfo
 
 def toonBezoekersFrame():
     FirstPage.pack_forget()
@@ -9,7 +10,8 @@ def toonBezoekersFrame():
     inloggennaam_label.grid(row=1, column=0, pady=5, padx=10)
     email_label.grid(row=2, column=0, pady=5, padx=10)
     terug_button1.grid(row=3, column=2)
-    root.geometry("300x100")
+    inloggen_button.grid(row=3, column=3)
+    root.geometry("400x100")
 
 def terugBezoekersNaarFirstpage():
     bezoekersFrame.pack_forget()
@@ -20,13 +22,30 @@ def terugBezoekersNaarFirstpage():
     FirstPage.pack()
 
 
+def bezoekersInfo():
+ 
+    bezoekersInfo_dict = {}
+    bez_naam = str(inloggennaam_entry.get())
+   
+    bez_email = str(email_entry.get())
+    try:
+        if (bez_naam or bez_email) == "":
+            bericht = "Voer uw naam en emailadres in A.U.B."
+            showinfo(title='Not a Valid Input', message=bericht)
 
+        elif bez_naam.isalpha() == False:
+            bericht = "Voer uw naam in"
+            showinfo(title='Not a Valid Input', message=bericht)
 
-    
+        else:
+            bezoekersInfo_dict[bez_naam] = bez_email
+            return bezoekersInfo_dict
+    except:
+        pass
+
 root= Tk()
 root.geometry("700x250")
 root.title("Rijksmuseum App")
-
 
 
 
@@ -40,21 +59,28 @@ label.image = photo
 label.pack(side=TOP, padx=10)
 
 bezoekersFrame = Frame(master=root)
+
 bezoekersFrame.pack(fill="both", expand=True)
 inloggennaam_entry = Entry(master=bezoekersFrame)
+
 email_entry = Entry(master=bezoekersFrame)
 inloggennaam_label = Label(master=bezoekersFrame, text="Naam")
+
 email_label = Label(master=bezoekersFrame, text="Email")
 terug_button1 = Button(master=bezoekersFrame, text="Terug", command=terugBezoekersNaarFirstpage)
 
+inloggen_button = Button(master=bezoekersFrame, text="Inloggen", command=bezoekersInfo)
 
 
 welkom_label = Label(master=FirstPage, text="Welkom in onze programma\nBen je een bezoeker of een galeriehouder?")
 welkom_label.pack(side=TOP, pady=10, padx=10)
+
 bezoekers_button = Button(master=FirstPage, text="Bezoeker", command=toonBezoekersFrame)
 bezoekers_button.pack(side=LEFT, pady=10, padx=10)
+
 ghouder_button = Button(master=FirstPage, text="Galleriehouder")
 ghouder_button.pack(side=RIGHT, pady=10, padx=10)
+
 FirstPage.pack()
 
 
