9abbfa30619c5be0a03bf5ccb93d603f5eafdac6
ib-2030 <55064204+ib-2030@users.noreply.github.com>
2019-10-30 12:07:10 +0100

    Add files via upload

--- welkomscreen.py
+++ welkomscreen.py
@@ -1,29 +1,57 @@
 from tkinter import *
 from PIL import ImageTk, Image 
+
+def toonBezoekersFrame():
+    FirstPage.pack_forget()
+    bezoekersFrame.pack
+    inloggennaam_entry.grid(row=1, column=1, pady=5, padx=10)
+    email_entry.grid(row=2, column=1, pady=5, padx=10)
+    inloggennaam_label.grid(row=1, column=0, pady=5, padx=10)
+    email_label.grid(row=2, column=0, pady=5, padx=10)
+    root.geometry("300x100")
+
+    
+
+
+    
 root= Tk()
 root.geometry("700x250")
-FirstPage = Frame(root)
 root.title("Rijksmuseum App")
 
 
+
+
+
+
+
+FirstPage = Frame(master=root)
 photo = PhotoImage(file= 'rm-logo.gif')
-label = Label(root, image = photo)
+label = Label(master=FirstPage, image = photo)
 label.image = photo
 label.pack(side=TOP, padx=10)
 
 
+bezoekersFrame = Frame(master=root)
+bezoekersFrame.pack(fill="both", expand=True)
+inloggennaam_entry = Entry(master=bezoekersFrame)
+email_entry = Entry(master=bezoekersFrame)
+inloggennaam_label = Label(master=bezoekersFrame, text="Naam")
+email_label = Label(master=bezoekersFrame, text="Email")
 
-welkom_label = Label(FirstPage, text="Welkom in onze programma\nBen je een bezoeker of een galeriehouder?")
+
+
+welkom_label = Label(master=FirstPage, text="Welkom in onze programma\nBen je een bezoeker of een galeriehouder?")
 welkom_label.pack(side=TOP, pady=10, padx=10)
-
-
-bezoekers_button = Button(FirstPage, text="Bezoeker")
+bezoekers_button = Button(master=FirstPage, text="Bezoeker", command=toonBezoekersFrame)
 bezoekers_button.pack(side=LEFT, pady=10, padx=10)
-
-ghouder_button = Button(FirstPage, text="Galleriehouder")
+ghouder_button = Button(master=FirstPage, text="Galleriehouder")
 ghouder_button.pack(side=RIGHT, pady=10, padx=10)
-
-def clicked():
-    pass
 FirstPage.pack()
+
+
+
+
+
+
+
 root.mainloop()
