dbe759be68a16faf9a46d003864da20f9d47a947
ib-2030 <55064204+ib-2030@users.noreply.github.com>
2019-10-31 20:15:05 +0100

    Add files via upload

--- welkomscreen.py
+++ welkomscreen.py
@@ -2,6 +2,7 @@ from tkinter import *
 from PIL import ImageTk, Image
 from tkinter.messagebox import showinfo
 import random
+import json
 
 bezoekersInfo_dict = {}
 ghoudersInfo_dict = {}
@@ -44,11 +45,9 @@ def terugRegFrameFirspage():
     FirstPage.pack()
 
 
-def kunststukkenLijstFrame():
-    pass
 
 def toonRegOfLogFrame():
-    root.geometry("350x310")
+    root.geometry("350x320")
 
     FirstPage.pack_forget()
     regOfLog.pack()
@@ -57,8 +56,64 @@ def toonRegOfLogFrame():
     log_button.grid(row=0,column=3, padx=50, pady=50)
     terug_button2.grid(row=1, column=3, pady= 100)
     
+
+
+
+def terugRegNaarRegofLog():
+    gh_inloggenFrame.pack_forget()
+    regOfLog.pack()
+    reg_button.grid(row=0,column=0, padx=50, pady=50)
+    log_button.grid(row=0,column=3, padx=50, pady=50)
+    terug_button2.grid(row=1, column=3, pady= 100)
+    root.geometry("350x320")
+
+
+
+def kunststukkenLijstFrame():
+    pass
+
+
+
+def gH_inlog_info():
+
+    gHI_naam = str(gHouder_naam.get())
+    gHI_email = str(gHouder_email.get())
+
+    if ghouderI_naam == "" or ghouderI_email == "":
+        print(ghoudersInfo_dict)
+    
+    """
+    try:
+        if ghouderI_naam == "" or ghouderI_email == "":
+            bericht = "Voer uw naam en emailadres in A.U.B."
+            showinfo(title='Not a Valid Input', message=bericht)
+
+        else:
+
+            ghouder_inloggen_info.append(ghouderI_naam, gehouderI_email)
+            if ghouder_inloggen_info[0] in ghoudersInfo_dict.keys():
+                if ghouderI_email == ghoudersInfo_dict ['ghouder email']:
+                    kustStukkenLijstFrame()
+                else:
+                    bericht = "Voer de juiste data in A.U.B."
+                    showinfo(title='Fout!', message=bericht)
+            else:
+                bericht = "Voer de juiste data in A.U.B."
+                showinfo(title='Fout!', message=bericht)
+            
+        return GhouderInfo_dict
+
+    except:
+        pass
+    """
+
+
+
+
+
+
+
 def registratieFrame():
-    root.geometry("350x160")
     regOfLog.pack_forget()
     regFrame.pack()
 
@@ -73,6 +128,20 @@ def registratieFrame():
     
     inloggen_button3.grid(row=3, column=2, padx=2, pady=5)
     terug_button3.grid(row=4, column=2, padx=2, pady=5)
+    root.geometry("350x190")
+    
+def toonGh_inloggenFrame():
+    regOfLog.pack_forget()
+    gh_inloggenFrame.pack()
+
+    inloggennaamGhouder_entry.grid(row=1, column=1, pady=5, padx=10)
+    emailGhouder_entry.grid(row=2, column=1, pady=5, padx=10)
+    inloggennaamGhouder_label.grid(row=1, column=0, pady=5, padx=10)
+    emailGhouder_label.grid(row=2, column=0, pady=5, padx=10)
+    terug_button_gh.grid(row=3, column=2)
+    inloggenGhouder_button.grid(row=3, column=3)
+    root.geometry("400x120")
+
 
 def gallerieBezoekersFrame():
     pass
@@ -98,17 +167,15 @@ def uitgeleendeKunststukkenFrame():
 
 
 def bezoekersInfo():
- 
-    bez_naam = str(inloggennaam_entry.get())
-   
+    bez_naam = str(inloggennaam_entry.get())   
     bez_email = str(email_entry.get())
     try:
-        if (bez_naam or bez_email) == "":
+        if bez_naam == "" or bez_email == "":
             bericht = "Voer uw naam en emailadres in A.U.B."
             showinfo(title='Not a Valid Input', message=bericht)
 
         elif bez_naam.isalpha() == False:
-            bericht = "Voer uw naam in"
+            bericht = "Voer uw eerste naam in"
             showinfo(title='Not a Valid Input', message=bericht)
 
         else:
@@ -116,7 +183,7 @@ def bezoekersInfo():
             bezoekersInfo_dict[bez_naam] = [bez_email, bez_code]
             with open('bezoekers-data.txt', 'a') as f:
                 for i, v in bezoekersInfo_dict.items():
-                    f.write('{}  {}'.format(i, v))
+                    f.write('{}  {}\n'.format(i, v))
             kustStukkenLijstFrame()
 
     except:
@@ -124,28 +191,42 @@ def bezoekersInfo():
 
 
 def gHouderInfo():
- 
+    ghouder_dict = {}
+
     gH_naam = str(gHouder_naam.get())
     gH_email = str(gHouder_email.get())
     gH_postcode = str(gHouder_postcode.get())
+    fn_ln = gH_naam.split()
+    
 
-
+    
+            
     try:
-        if  gH_naam == "" or gH_email == "" or gH_postcode == "":
+        if gH_naam == "" or gH_email == "" or gH_postcode == "":
             bericht = "Unvalid input. Probeer nog een keer!"
-            showinfo(title='Not a Valid Input', message=bericht)
-
-        elif gH_naam.isalpha() == False:
-            bericht = "Voer uw naam in"
-            showinfo(title='Not a Valid Input', message=bericht)
+            showinfo(title='Not a Valid Input', message=bericht)         
 
         else:
-            ghoudersInfo_dict[gH_naam] = [gH_email, gH_postcode]
-            kunststukkenLijstFrame()
+            ghouder_dict['ghouder naam'] = [gH_naam]
+            ghouder_dict['ghouder email'] = [gH_email]
+            ghouder_dict['ghouder postcode'] = [gH_postcode]
+            gh_firstname = fn_ln[0]
+            ghoudersInfo_dict[gh_firstname] = ghouder_dict
+            json_gh_data = json.dumps(ghoudersInfo_dict, indent=4)
+            
+            with open('gallerihouder-data.json', 'w') as json_file:
+                json_file.write(json_gh_data)
+            
+            print(ghouder_dict)
+            print(ghoudersInfo_dict)
+            
+            terugRegFrameFirspage()
+            toonRegOfLogFrame()
+        
 
     except:
-        print('Lel')
-        
+        pass
+            
 
 
 
@@ -194,6 +275,74 @@ ButtonNaardewebsite.pack()
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+gh_inloggenFrame = Frame(master=root)
+gh_inloggenFrame.pack(fill="both", expand=True)
+inloggennaamGhouder_entry = Entry(master=gh_inloggenFrame)
+emailGhouder_entry = Entry(master=gh_inloggenFrame)
+inloggennaamGhouder_label = Label(master=gh_inloggenFrame, text="Naam")
+emailGhouder_label = Label(master=gh_inloggenFrame, text="Email")
+terug_button_gh = Button(master=gh_inloggenFrame, text="Terug", command=terugRegNaarRegofLog)
+inloggenGhouder_button = Button(master=gh_inloggenFrame, text="Inloggen", command="")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #######################################################################
 
 
@@ -202,7 +351,7 @@ regOfLog.pack(fill='both', expand=True)
 
 
 reg_button = Button(master=regOfLog, text="registratie", command=registratieFrame)
-log_button = Button(master=regOfLog, text="inloggen", command="")
+log_button = Button(master=regOfLog, text="inloggen", command=toonGh_inloggenFrame)
 terug_button2 = Button(master=regOfLog, text="Terug", command=terugRegOfLogNaarFirspage)
 
 ######################################################################
@@ -215,7 +364,7 @@ gHouder_naam = Entry(master=regFrame)
 gHouder_email = Entry(master=regFrame)
 gHouder_postcode= Entry(master=regFrame)
 
-gHouder_naam_label = Label(master=regFrame, text="Naam")
+gHouder_naam_label = Label(master=regFrame, text="Vol naam")
 gHouder_email_label = Label(master=regFrame, text="Email")
 gHouder_postcode_label = Label(master=regFrame, text="Postcode")
 
@@ -260,7 +409,7 @@ ghouder_button = Button(master=FirstPage, text="Galleriehouder", command=toonReg
 ghouder_button.pack(side=RIGHT, pady=10, padx=10)
 
 
-########################################################################
+ ########################################################################
 
 FirstPage.pack()
 
