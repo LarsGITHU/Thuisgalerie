0507902f6f308a795eb2a6449457ce95562ccd23
Fare-alkhadrawy <57097229+Fare-alkhadrawy@users.noreply.github.com>
2019-10-31 17:47:30 +0100

    dit json file voor foto"s en titels

--- json.py
+++ json.py
@@ -1,5 +1,12 @@
 import http.client, urllib.parse, json
+import json
+from tkinter import *
+from webbrowser import *
 
+root = Tk()
+
+
+werkstukken_dict = {}
 key = 'Ostta0g5'
 
 try:
@@ -11,7 +18,25 @@ try:
     responsetext = response.read()
     conn.close()
     resjson = json.loads(responsetext)
-    print(resjson)
+   # print(informatie(resjson))
+    artobject= resjson['artObjects']
+    for object in artobject:
+     werkstukken_dict[object['longTitle']] =  (object['webImage']['url'])
 
 except Exception as e:
-    print("Fout: {} {}".format(e.errno, e.strerror))
+    print(f"Fout: {e.errno} {e.strerror}")
+
+
+result =[]
+eval_link = lambda x: (lambda e: open(x))
+for key in werkstukken_dict:
+    value = werkstukken_dict[key]
+    print(key,value)
+    result.append(Label(root, text=key, fg="blue", cursor="hand2"))
+    result[-1].bind("<Button-1>", eval_link(value))
+#    for i in range(10):
+#        result[-1].bind("<Button-1>", lambda e: open(value))
+    result[-1].pack()
+
+print(werkstukken_dict)
+root.mainloop()
